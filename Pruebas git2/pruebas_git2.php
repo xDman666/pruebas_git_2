5bcbfab72ca3be6eb2214968d54a3dd22a6ba9dd
Albert Bursan <sipsychosexy@gmail.com>
2021-09-15 11:54:24 +0200

    ramaAlbert cambio en pruebas_git2.php

--- Pruebas git2/pruebas_git2.php
+++ Pruebas git2/pruebas_git2.php
@@ -2,6 +2,6 @@
 
 echo "hola2 mundo";
 
-echo "esto es un cambio que he hecho en feature de develop";
-
 echo "borrado de primer hola mundo y creacion de archivo";
+
+echo "subir cambio de feature/Albert por rama a develop";
